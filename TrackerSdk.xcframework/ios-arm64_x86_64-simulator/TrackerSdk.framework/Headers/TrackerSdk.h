3a8d4171d0e4c8471bb983f4d7371678d2060e62
Hasan Gözüm <hasangzm@gmail.com>
2024-05-31 15:59:48 +0300

    first

---
+++ TrackerSdk.xcframework/ios-arm64_x86_64-simulator/TrackerSdk.framework/Headers/TrackerSdk.h
@@ -0,0 +1,20 @@
+//
+//  TrackerSdk.h
+//  TrackerSdk
+//
+//  Created by Hasan Gözüm on 6.05.2024.
+//
+
+#import <Foundation/Foundation.h>
+#import <TrackerSdk/DeviceUID.h>
+
+
+//! Project version number for TrackerSdk.
+FOUNDATION_EXPORT double TrackerSdkVersionNumber;
+
+//! Project version string for TrackerSdk.
+FOUNDATION_EXPORT const unsigned char TrackerSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TrackerSdk/PublicHeader.h>
+
+
